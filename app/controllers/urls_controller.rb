54bc3f316239f6fe253ddab1a94b9aec63fc41a3
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-06-01 09:53:40 +0300

    feat: add redirect action

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,6 +4,7 @@ class UrlsController < ApplicationController
   end
 
   def create
+    byebug
     @url = Url.new(url_params)
     @url.shortened = SecureRandom.urlsafe_base64(6)
 
@@ -11,6 +12,15 @@ class UrlsController < ApplicationController
       render :new
     else
       render :new
+    end
+  end
+
+  def redirect
+    @url = Url.find_by(shortened: params[:short_url])
+    if @url.present?
+      redirect_to @url.original, allow_other_host: true
+    else
+      redirect_to root_path, notice: 'Link not found'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
-  resources :urls, only: [:new, :create, :show]
   root 'urls#new'
+
+  get "up" => "rails/health#show", as: :rails_health_check
+  get '/:short_url', to: 'urls#redirect'
+
+  resources :urls, only: [:new, :create, :show]
+
 end
